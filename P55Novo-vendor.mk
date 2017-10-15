bcf809aba8e826d3be874097d3f59fb13cb31a2f
shivom007 <yashpaljoshi76@gmail.com>
2017-10-15 16:04:56 +0530

    Vendor blobs P55 Novo

---
+++ P55Novo-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55Novo/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55Novo/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55Novo/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55Novo/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55Novo/proprietary/vendor,system/vendor)
